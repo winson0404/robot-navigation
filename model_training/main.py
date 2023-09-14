30a57a67f18b39e8320b57e822d911b06e9ec99b
winson0404 <weixiong8489@gmail.com>
2023-09-15 07:02:22 +0800

    updated main.py

--- model_training/main.py
+++ model_training/main.py
@@ -6,16 +6,44 @@ import numpy as np
 from utils.util import full_frame_preprocess, full_frame_postprocess, crop_roi_image, draw_image
 from omegaconf import OmegaConf
 from utils.transform import Compose
+from utils.data_processor import DataProcessor
 import socket
 import time
 
 # hand_model_path = "../data/models/YoloV7_Tiny.onnx"
-model_path = r"D:\Wei_Xiong\projects\table-navigation\model_training\output\CustomNetV2\two_adam_80\best_model.onnx"
+model_path = r"/home/weixiong0404/Documents/workspace/table-navigation/model_training/output/CustomNetV2/two_adam_80/best_model.onnx"
 provider = ['CPUExecutionProvider']
 threshold = 0.5
-conf = OmegaConf.load("configs/CustomNet/default.yaml")
+conf = OmegaConf.load("configs/CustomNetv2/default.yaml")
 input_shape=(3, conf.dataset.image_size[0], conf.dataset.image_size[1])
 target_dict = {i: label for i, label in enumerate(conf.dataset.targets)}
+
+def draw_rectangle(frame, x, y, w, h):
+    x = int(x)
+    y = int(y)
+    w = int(w)
+    h = int(h)
+    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+    return frame
+
+def post_process(frame, prediction):
+    frame = draw_rectangle(frame, 0, 240*0.6, 320, 240*0.4)
+    if prediction == "no_objects":
+        return frame
+    elif prediction == "left":
+        frame = draw_rectangle(frame, 10, 240*0.6+5, 100, 240*0.4-10)
+    elif prediction == "left_mid":
+        frame = draw_rectangle(frame, 10, 240*0.6+5, 200, 240*0.4-10)
+    elif prediction == "middle":
+        frame = draw_rectangle(frame, 110, 240*0.6+5, 100, 240*0.4-10)
+    elif prediction == "right":
+        frame = draw_rectangle(frame, 210, 240*0.6+5, 100, 240*0.4-10)
+    elif prediction == "right_mid":
+        frame = draw_rectangle(frame, 110, 240*0.6+5, 200, 240*0.4-10)
+    elif prediction == "all":
+        frame = draw_rectangle(frame, 10, 240*0.6+5, 300, 240*0.4-10)
+        
+    return frame
 
 class TCP_Client():
     def __init__(self, host, port:int) -> None:
@@ -50,12 +78,13 @@ class TCP_Client():
             
 if __name__ == "__main__":
     
-    camera_name = "/dev/video2"
-    cap = cv2.VideoCapture(camera_name)
+    camera_name = "/dev/video0"
+    cap = cv2.VideoCapture(0)
     inf_session = ort.InferenceSession(model_path, providers=provider)
     model_out_name = [i.name for i in inf_session.get_outputs()]
     transform = Compose(input_shape[1:])
-    client = TCP_Client('127.0.0.1', 12345)
+    # client = TCP_Client('127.0.0.1', 12345)
+    processor = DataProcessor()
         
     while cap.isOpened():
         ret, frame = cap.read()
@@ -66,16 +95,23 @@ if __name__ == "__main__":
                 # breakpoint()
                 #convert from tensor to numpy
                 # np_image = transform(frame).unsqueeze(0).numpy()
-                out = inf_session.run(model_out_name, {'images': transform(frame).unsqueeze(0).numpy()})[0]
+                # resize frame 
+                frame = cv2.resize(frame, (320, 240))
+                roi = processor.crop_roi(frame, 0, 240*0.6, 320, 240*0.4)
+                roi = cv2.resize(roi, (96, 320))
+                out = inf_session.run(model_out_name, {'images': transform(roi).unsqueeze(0).numpy()})[0]
                 # bbox, score = full_frame_postprocess(out, ratio, dwdh, threshold)
                 # if score >= threshold:
                 #     cropped_image = crop_roi_image(frame, bbox, (224, 224))
                 #     out = model_path.run(gesture_out_name, {'images': cropped_image/255})[0]
                 #     gesture = target_dict[out.argmax()]
                 #     draw_image(frame, bbox, score, gesture)
-                client.Send('0', out.argmax())
+                # client.Send('0', out.argmax())
                 direction = target_dict[out.argmax()]
                 # print(direction)
+                # breakpoint()
+                frame = post_process(frame, direction)
+                frame = cv2.resize(frame, (640, 480))
                 cv2.imshow(camera_name, frame)
                 # time.sleep(2)
         except Exception as e:
